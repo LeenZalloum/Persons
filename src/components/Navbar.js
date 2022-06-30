f5ec1b2c86c4e25c423dc6e0946d927658aff4af
LeenZalloum <zalloum_leen@yahoo.com>
2022-06-30 14:31:06 +0300

    modify added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return ( 
         <nav className="navbar">
+            <h2>Persons Website</h2>
+            <div className="links">
             <Link to="/">Home</Link>
             <Link to="/addPerson">Add Person</Link>
+            </div>
         </nav>
      );
 }
